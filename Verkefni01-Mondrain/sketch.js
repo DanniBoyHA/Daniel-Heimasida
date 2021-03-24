e6e4ac0975c34909244146a46282b2a8116bd343
DanniBoyHA <dankarason@gmail.com>
2021-03-24 14:04:54 +0000

    verkefni 1

---
+++ Verkefni01-Mondrain/sketch.js
@@ -0,0 +1,32 @@
+// Hér kemur kóðinn þinn:
+
+function setup(){
+    createCanvas(600,600);
+    background(0,255,0);
+    strokeWeight(8);
+    stroke(51);
+    //rauði kassinn
+    fill(197,49,37);
+    rect(0,0,260,350);
+    //hvíti kassinn uppi
+    fill(237,237,234);
+    rect(260,0,340,350);
+    //hvíti kassinn niðri
+    fill(232,233,228);
+    rect(0,350,260,250);
+    //Gráhvíti kassinn
+    fill(230,230,230);
+    rect(260,350,280,230);
+    //blái kassinn
+    fill(0,0,255);
+    rect(540,350,60,100);
+    //hvíti kassinn niðri
+    fill(229,230,225);
+    rect(540,450,60,150);
+    //guli kassinn
+    fill(240,212,48);
+    rect(260,580,150,20);
+    //svarti kassinn
+    fill(34,34,35);
+    rect(410,580,130,20);
+}
