6a6c9133a497e6034d875af88969b2479dfcaf72
DanniBoyHA <dankarason@gmail.com>
2021-04-08 10:48:32 +0000

    verkefni 2 lifandi mynd

--- Verkefni02-LifandiMynd/sketch.js
+++ Verkefni02-LifandiMynd/sketch.js
@@ -6,12 +6,27 @@ function setup(){
     noStroke();
     fill(0,50,0);
     rect(0,500,800,200);
+    //eldfjall 1
     fill(1,1,1);
+    triangle(250,200,250,500,100,500);
+    triangle(350,200,350,500,500,500);
+    fill(50,50,50);
+    rect(250,200,100,400);
+    //eldfjall 2
+    fill(1,1,1);
+    triangle(550,300,550,500,400,500);
+    triangle(600,300,600,500,750,500);
+    fill(50,50,50);
+    rect(550,300,50,300);
 }
 
 function draw() {
-  setup();
+  //setup();
   frameRate(3);
-  fill(244,0,0);
-  ellipse(random(350,450), random(100,500), 20,20);
+  fill(140,0,0);
+  //eldfall 1
+  ellipse(random(250,350), random(180,600), 20,20);
+  ellipse(random(250,350), random(200,600), 20,20);
+  //eldfjall 2
+  ellipse(random(550,600), random(280,600), 20,20);
 }
